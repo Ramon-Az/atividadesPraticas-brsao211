738ac84dde168a0f577294de66b296376f88e389
Ramon Azevedo <ramon.azvdo@gmail.com>
2026-01-08 19:31:07 -0400

    demais questões - Status: atividade concluída

---
+++ atividadePratica01/questao04.py
@@ -0,0 +1,7 @@
+produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+total = preco_unitario * quantidade
+
+print(f"Informações: \nProduto: {produto} \nPreço unitário: R$ {preco_unitario} \nQuantidade: {quantidade} \nTotal da compra: R$ {total}")
