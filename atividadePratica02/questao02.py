0caa1aa73acef8a4b8008bfe74da65eec67bfb79
Ramon Azevedo <ramon.azvdo@gmail.com>
2026-01-09 19:21:23 -0400

    .

--- atividadePratica02/questao02.py
+++ atividadePratica02/questao02.py
@@ -3,6 +3,7 @@ nome_produto = "Camiseta"
 preco_produto = 50.00
 percentagem_desconto = 0.20
 
-preco_final = preco_produto - (preco_produto * percentagem_desconto)
+desconto = preco_produto * percentagem_desconto
+preco_final = preco_produto - (desconto)
 
-print(f"Informações da compra: \nProduto: {nome_produto} \nPreço: R${preco_produto:.2f} \nDesconto: {percentagem_desconto * 100:.0f}% \nPreço final: R${preco_final:.2f}")
+print(f"Informações da compra: \nProduto: {nome_produto} \nPreço: R${preco_produto:.2f} \nDesconto: {percentagem_desconto * 100:.0f}% é igual a R${desconto:.2f}\nPreço final: R${preco_final:.2f}")
